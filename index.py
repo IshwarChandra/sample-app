d1ec4198cdd67387f0637724832e7c1e4af26606
Ishwar Chandra <ishwar.chandra@NDI-LAP-686.local>
2018-09-13 16:53:15 +0530

    j

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello IC World',
+        'output': 'Hello World 1',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
