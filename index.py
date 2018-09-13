37b92e32c146b59fc942d85a7e1370cff17292ee
Ishwar Chandra <kontact.ishu@gmail.com>
2018-09-13 18:57:43 +0530

    6

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World 5',
+        'output': 'Hello World 6',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
