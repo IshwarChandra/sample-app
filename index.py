160cf2f06c5654f64f9ad45d27ca19466b30c499
Ishwar Chandra <kontact.ishu@gmail.com>
2018-09-18 17:35:09 +0530

    4

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'PR 3',
+        'output': 'PR 4',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
